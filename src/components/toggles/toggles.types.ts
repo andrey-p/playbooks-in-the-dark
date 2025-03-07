34ad036ad6e9e173388faed620d9cad022c3c453
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-03-07 20:41:33 +0000

    rejig toggle buttons so they're findable by react-testing-library

--- src/components/toggles/toggles.types.ts
+++ src/components/toggles/toggles.types.ts
@@ -2,7 +2,7 @@ export type ToggleProps = {
   size?: number;
   filled?: boolean;
   highlighted?: boolean;
-  onMouseEnter?: (e: React.MouseEvent<HTMLDivElement>) => void;
-  onMouseLeave?: (e: React.MouseEvent<HTMLDivElement>) => void;
-  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  onMouseEnter?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onMouseLeave?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
