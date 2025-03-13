a2c98ab65a596801ae70f0fdf34e83f2eb949f71
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-03-13 11:07:06 +0000

    change home route so you pick a system first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,20 @@
 //import styles from "./page.module.css";
 import Link from 'next/link';
 import { getJson } from '@/lib/system-data';
-import type { PlaybookDefinition } from '@/types';
+import type { System } from '@/types';
+import systemsJson from '@/systems/systems.json';
 
 export default function Home() {
-  const systemCharactersData = getJson(
-    'bitd',
-    'character'
-  ) as PlaybookDefinition;
+  const availableSystems = systemsJson.systems.map((system) => {
+    return getJson(system.id, 'system') as System;
+  });
 
   return (
     <div>
       New character:
-      {systemCharactersData.playbooks.map((playbook: string) => (
-        <Link key={playbook} href={`bitd/character/${playbook}`}>
-          {playbook}
+      {availableSystems.map((system) => (
+        <Link key={system.id} href={`/${system.id}`}>
+          {system.name}
         </Link>
       ))}
     </div>
