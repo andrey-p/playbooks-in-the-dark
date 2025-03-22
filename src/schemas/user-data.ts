0864d214bd2dcc63d83f7a04c211ffd6cfc212bb
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-03-22 19:25:57 +0000

    remove catchall on user-data schema

--- src/schemas/user-data.ts
+++ src/schemas/user-data.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
 
-const UserData = z
-  .object({
-    id: z.string().or(z.undefined()),
-    systemId: z.string(),
-    playbookType: z.string(),
-    playbookId: z.string(),
-    modules: z.record(z.string(), z.any())
-  })
-  .catchall(z.any());
+const UserData = z.object({
+  id: z.string().or(z.undefined()),
+  systemId: z.string(),
+  playbookType: z.string(),
+  playbookId: z.string(),
+  modules: z.record(z.string(), z.any())
+});
 
 export default UserData;
