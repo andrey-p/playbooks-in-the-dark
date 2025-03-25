b12589adfd5e786688f33dc789f0f5cfad0e8752
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-03-25 18:55:12 +0000

    generate reducer action type automatically based on the unified user value schemas

--- src/components/playbook-modules/all-schemas.ts
+++ src/components/playbook-modules/all-schemas.ts
@@ -1,4 +1,5 @@
 // kept separate from all-modules.ts so this file can be loaded by server code
+import { z } from 'zod';
 
 import * as TextFieldSchemas from './text-field/text-field.schema';
 import * as TextAreaSchemas from './text-area/text-area.schema';
@@ -11,6 +12,7 @@ import * as HarmSchemas from './harm/harm.schema';
 import * as MultiTrackerSchemas from './multi-tracker/multi-tracker.schema';
 import * as ContactsSchemas from './contacts/contacts.schema';
 import * as ClaimsSchemas from './claims/claims.schema';
+import * as CohortsSchemas from './cohorts/cohorts.schema';
 
 export const schemasByModuleType = {
   textField: TextFieldSchemas,
@@ -23,5 +25,22 @@ export const schemasByModuleType = {
   harm: HarmSchemas,
   multiTracker: MultiTrackerSchemas,
   contacts: ContactsSchemas,
-  claims: ClaimsSchemas
+  claims: ClaimsSchemas,
+  cohorts: CohortsSchemas
+};
+
+export const getUnifiedUserValueSchema = () => {
+  // pull in schemas for all kinds of user value (e.g. data that will be saved to the DB)
+  // and create a big zod schema that checks for all of them
+  const userValueSchemas = Object.values(schemasByModuleType).map(
+    (schemas) => schemas.UserValue
+  );
+  const unifiedUserValueSchema = userValueSchemas.reduce(
+    (acc: z.ZodTypeAny, val: z.ZodTypeAny) => {
+      return acc.or(val);
+    },
+    z.void()
+  );
+
+  return unifiedUserValueSchema;
 };
--- src/reducers/user-data-action.ts
+++ src/reducers/user-data-action.ts
@@ -1,26 +1,14 @@
+import { z } from 'zod';
+import { getUnifiedUserValueSchema } from '@/components/playbook-modules/all-schemas';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const unifiedUserValueSchema = getUnifiedUserValueSchema();
+
 type Action =
   | {
       type: 'set_value';
       key: string;
-      value:
-        | string
-        | { value: string | null }
-        | { contacts: Record<string, number> }
-        | { values: Record<string, number> }
-        | {
-            actionRatings: Record<string, number>;
-            attributeXp: Record<string, number>;
-          }
-        | { text: string }
-        | { harmsTaken: { text: string; column: number; level: number }[] }
-        | Record<string, number>
-        | {
-            items: Record<string, number>;
-            load?: string | null | undefined;
-          }
-        | {
-            selected: Record<string, number>;
-          };
+      value: z.infer<typeof unifiedUserValueSchema>;
     }
   | {
       type: 'set_id';
