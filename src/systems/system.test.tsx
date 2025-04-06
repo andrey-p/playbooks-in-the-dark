012d765150d236ece42e47eb30cf015af2e5e666
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-06 09:40:58 +0100

    fix type warnings in system test

--- src/systems/system.test.tsx
+++ src/systems/system.test.tsx
@@ -3,6 +3,8 @@ import Renderer from '@/components/playbook-modules/renderer';
 import { render } from '@testing-library/react';
 import systemsJson from './systems.json';
 
+import { PlaybookData, System, PlaybookDefinition } from '@/schemas';
+
 // a lot of the data is checked at runtime, when the sheet is rendered
 // and <Renderer /> will immediately crash if it doesn't like something
 //
@@ -16,14 +18,17 @@ describe('system data check', () => {
     const systemId = system.id;
 
     describe(systemId, () => {
-      // maybe system data will be passed down at some point?
-      const systemData = getJson(systemId, 'system');
+      const systemData = System.parse(getJson(systemId, 'system'));
 
       systemData.playbookTypes.forEach((playbookType: string) => {
-        const playbookDefinition = getJson(systemId, playbookType);
+        const playbookDefinition = PlaybookDefinition.parse(
+          getJson(systemId, playbookType)
+        );
 
         playbookDefinition.playbooks.forEach((playbookId: string) => {
-          const playbookData = getJson(systemId, playbookId);
+          const playbookData = PlaybookData.parse(
+            getJson(systemId, playbookId)
+          );
 
           test(`playbook ${playbookId} should render OK`, () => {
             render(
