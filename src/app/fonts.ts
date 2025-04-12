892f4acd677a3606876820ff3ed60f926a825290
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-12 08:08:29 +0100

    add display font for AFOD

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,4 +1,4 @@
-import { Goblin_One, EB_Garamond } from 'next/font/google';
+import { Goblin_One, EB_Garamond, Rubik_Dirt } from 'next/font/google';
 
 const goblinOne = Goblin_One({
   variable: '--font-goblin-one',
@@ -14,7 +14,14 @@ const garamond = EB_Garamond({
   fallback: ['Times New roman', 'serif']
 });
 
-const fonts = [goblinOne, garamond];
+const rubikDirt = Rubik_Dirt({
+  variable: '--font-rubik-dirt',
+  subsets: ['latin'],
+  weight: '400',
+  fallback: ['Impact', 'sans-serif']
+});
+
+const fonts = [goblinOne, garamond, rubikDirt];
 
 export const getFontClassName = (): string => {
   return fonts.map((font) => font.variable).join(' ');
