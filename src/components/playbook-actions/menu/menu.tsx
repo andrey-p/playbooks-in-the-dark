a7ec9d50949263088bd0f87eb071790f8ec5bd78
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-14 10:12:34 +0100

    add separate links for editable and read-only share links

--- src/components/playbook-actions/menu/menu.tsx
+++ src/components/playbook-actions/menu/menu.tsx
@@ -53,32 +53,52 @@ export default function Menu(props: Props) {
         <Button onClick={onClose} label='Close menu' icon={<FiX />} />
       </div>
 
-      {userData.id && (
+      {(userData.id || userData.shareId) && (
         <ul className={styles.menuList}>
-          <li className={styles.menuItem}>
-            <CopyMenuItem userDataId={userData.id} />
-          </li>
-          <li className={styles.menuItem}>
-            <DeleteMenuItem userDataId={userData.id} />
-          </li>
+          {userData.id && (
+            <li className={styles.menuItem}>
+              <CopyMenuItem
+                text='Copy editable link'
+                path={`${userData.systemId}/${userData.playbookType}/${userData.playbookId}/${userData.id}`}
+              />
+            </li>
+          )}
+          {userData.shareId && (
+            <li className={styles.menuItem}>
+              <CopyMenuItem
+                text='Copy read-only link'
+                path={`share/${userData.shareId}`}
+              />
+            </li>
+          )}
+          {userData.id && (
+            <li className={styles.menuItem}>
+              <DeleteMenuItem userDataId={userData.id} />
+            </li>
+          )}
         </ul>
       )}
 
       <h3 className={styles.explainerHeading}>How does saving work?</h3>
       <p className={styles.explainerP}>
-        The first time you save a playbook, it gets assigned an ID, and you can
-        share it to other devices or your friends and enemies.
+        After saving a playbook for the first time, you can copy either an
+        editable link or a read-only link.
+      </p>
+      <p className={styles.explainerP}>
+        The editable link allows anyone to update (and potentially delete!) your
+        playbook. Only send it to your close allies and your inner circle.
+      </p>
+      <p className={styles.explainerP}>
+        The read-only link allows anyone to look at, but not mess with, your
+        playbook. Send it to acquaintances and potential turncoats.
       </p>
       <p className={styles.explainerP}>
         The linked playbooks on the homepage are stored in your browser&apos;s
         local storage.
       </p>
       <p className={styles.explainerP}>
-        There is currently no way to assert ownership of a saved playbook. For
-        now, anyone with the link can edit or, yes, delete your playbook.
-      </p>
-      <p className={styles.explainerP}>
-        Hate this? Go and shout at Andrey{' '}
+        There is currently no way to assert ownership of a saved playbook. Hate
+        this? Go and shout at Andrey{' '}
         <a
           target='_blank'
           href='https://github.com/andrey-p/playbooks-in-the-dark'
