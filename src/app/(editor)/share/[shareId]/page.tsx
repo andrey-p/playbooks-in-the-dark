c09d30578dac4e60400aba5bc54a88f63f773ee8
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-14 08:49:28 +0100

    change share endpoint so it renders the editor directly

--- src/app/(editor)/share/[shareId]/page.tsx
+++ src/app/(editor)/share/[shareId]/page.tsx
@@ -1,4 +1,12 @@
-import { redirect, notFound } from 'next/navigation';
+import { notFound } from 'next/navigation';
+import { getJson } from '@/lib/system-data';
+import { NotFoundError } from '@/lib/errors';
+import {
+  PlaybookData as PlaybookDataSchema,
+  PlaybookDefinition as PlaybookDefinitionSchema,
+  System as SystemSchema
+} from '@/schemas';
+import PlaybookEditor from '../../components/playbook-editor';
 import { getPlaybookByShareId } from '@/lib/store';
 
 type Props = {
@@ -8,11 +16,42 @@ type Props = {
 export default async function Page(props: Props) {
   const { shareId } = await props.params;
 
-  const data = await getPlaybookByShareId(shareId);
+  const userData = await getPlaybookByShareId(shareId);
 
-  if (!data) {
+  if (!userData) {
     return notFound();
   }
 
-  redirect(`/${data.systemId}/${data.playbookType}/${data.playbookId}/${id}`);
+  let systemData;
+  let playbookData;
+  let playbookDefinition;
+
+  try {
+    systemData = SystemSchema.parse(getJson(userData.systemId, 'system'));
+    playbookData = PlaybookDataSchema.parse(
+      getJson(userData.systemId, userData.playbookId)
+    );
+    playbookDefinition = PlaybookDefinitionSchema.parse(
+      getJson(userData.systemId, userData.playbookType)
+    );
+  } catch (e) {
+    if (e instanceof NotFoundError) {
+      return notFound();
+    }
+
+    throw e;
+  }
+
+  // don't give out the main ID when sharing
+  delete userData.id;
+
+  return (
+    <PlaybookEditor
+      systemData={systemData}
+      playbookData={playbookData}
+      playbookDefinition={playbookDefinition}
+      userData={userData}
+      readOnly
+    />
+  );
 }
