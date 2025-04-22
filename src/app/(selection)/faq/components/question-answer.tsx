e74a53f1ff89120a1dced74e2b326b03c1f37e2c
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-22 10:49:31 +0100

    add ability to deep link to FAQ questions

--- src/app/(selection)/faq/components/question-answer.tsx
+++ src/app/(selection)/faq/components/question-answer.tsx
@@ -1,4 +1,5 @@
 import styles from './question-answer.module.css';
+import { FiLink } from 'react-icons/fi';
 
 type Props = {
   id: string;
@@ -11,7 +12,16 @@ export default function QuestionAnswer(props: Props) {
 
   return (
     <div className={styles.container}>
-      <h3 className={styles.question}>{question}</h3>
+      <h3 className={styles.question} id={id}>
+        {question}
+      </h3>
+      <a
+        className={styles.link}
+        href={`#${id}`}
+        aria-label='Direct link to this question'
+      >
+        <FiLink />
+      </a>
       <div
         className={styles.answer}
         dangerouslySetInnerHTML={{ __html: answer }}
