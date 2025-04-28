5eee71ef3962365ba74168734d8d88b1e44ae630
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-28 12:18:40 +0100

    comments

--- src/app/(selection)/components/saved-playbooks.tsx
+++ src/app/(selection)/components/saved-playbooks.tsx
@@ -16,11 +16,15 @@ export default function SavedPlaybooks() {
       throw new Error("Couldn't find system: " + playbook.systemId);
     }
 
+    // playbook.name and playbook.description are verbatim strings that
+    // are no longer set for newly saved character sheets
+    // they're still checked here for backwards compatibility
     let name = playbook.name || playbook.modules?.name?.text;
     const description =
       playbook.description ||
       t(`${system.translationNamespace}.Playbooks.${playbook.playbookId}`);
 
+    // no name, show this as e.g. "an unnamed cutter"
     if (!name) {
       name = t(
         `${system.translationNamespace}.Playbooks.unnamed.${playbook.playbookType}`
