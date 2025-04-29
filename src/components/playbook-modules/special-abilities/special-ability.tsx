feab6aece84bc946765cd4bb891da0510f118225
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-04-29 08:38:39 +0100

    add slot support for special ability descriptions

--- src/components/playbook-modules/special-abilities/special-ability.tsx
+++ src/components/playbook-modules/special-abilities/special-ability.tsx
@@ -1,18 +1,25 @@
 import { z } from 'zod';
 import SimpleTracker from '@/components/playbook-elements/trackers/simple-tracker';
 import { SpecialAbility as SpecialAbilitySchema } from './special-abilities.schema';
+import { SlotValue as SlotValueSchema } from '@/components/playbook-elements/slotted-text/slotted-text.schema';
+import SlottedText from '@/components/playbook-elements/slotted-text/slotted-text';
 import styles from './special-ability.module.css';
 import Description from '@/components/playbook-elements/description/description';
 import { useTranslations } from 'next-intl';
+
+type SlotValueType = z.infer<typeof SlotValueSchema>;
 
 type Props = {
   specialAbility: z.infer<typeof SpecialAbilitySchema>;
   selected?: number;
   onSelect: (selected: number) => void;
+  slotValues?: SlotValueType;
+  onSlotUpdate: (newSlotValue: SlotValueType) => void;
 };
 
 export default function SpecialAbility(props: Props) {
-  const { specialAbility, selected, onSelect } = props;
+  const { specialAbility, selected, slotValues, onSlotUpdate, onSelect } =
+    props;
   const t = useTranslations();
 
   return (
@@ -27,7 +34,16 @@ export default function SpecialAbility(props: Props) {
       </div>
       <div>
         <span className={styles.name}>{t(specialAbility.name)}: </span>
-        <Description text={specialAbility.description} />
+        {specialAbility.slots ? (
+          <SlottedText
+            text={specialAbility.description}
+            slots={specialAbility.slots}
+            values={slotValues}
+            onUpdate={onSlotUpdate}
+          />
+        ) : (
+          <Description text={specialAbility.description} />
+        )}
       </div>
     </div>
   );
