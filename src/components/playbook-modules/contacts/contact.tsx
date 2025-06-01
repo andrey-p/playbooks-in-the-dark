b8c35221bd7d26cfcedb7eb5f377a613c0876d3c
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-06-01 15:01:08 +0100

    update contacts to use wrapper

--- src/components/playbook-modules/contacts/contact.tsx
+++ src/components/playbook-modules/contacts/contact.tsx
@@ -4,7 +4,9 @@ import { Contact as ContactSchema } from './contacts.schema';
 import { SlotValue as SlotValueSchema } from '@/components/playbook-elements/slotted-text/slotted-text.schema';
 import SlottedText from '@/components/playbook-elements/slotted-text/slotted-text';
 import Toggle from '@/components/playbook-elements/toggle/toggle';
+import RadioControlWrapper from '@/components/playbook-elements/radio-control-wrapper/radio-control-wrapper';
 import styles from './contact.module.css';
+import { useTranslations } from 'next-intl';
 
 type ContactType = z.infer<typeof ContactSchema>;
 type SlotValueType = z.infer<typeof SlotValueSchema>;
@@ -29,9 +31,15 @@ export default function Contact(props: Props) {
   } = props;
   const [highlighted, setHighlighted] = useState<number | null>(null);
   const consistentId = useId();
+  const t = useTranslations();
 
   return (
-    <div>
+    <RadioControlWrapper
+      name={consistentId}
+      zeroSelected={relationship === 0}
+      onZeroSelect={() => onRelationshipUpdate(contact.id, 0)}
+      label={contact.name}
+    >
       <div className={variant === 'upDown' ? styles.up : styles.neutral}>
         <Toggle
           type='triangle'
@@ -41,11 +49,20 @@ export default function Contact(props: Props) {
           controlType='radio'
           controlProps={{
             name: consistentId,
+            'aria-label':
+              variant === 'upDown'
+                ? t('UI.ModulesShared.contactsPositive')
+                : t('UI.ModulesShared.contactsNeutral'),
             checked: relationship === 1,
             onMouseEnter: () => setHighlighted(1),
             onMouseLeave: () => setHighlighted(null),
             onChange: () => {
-              onRelationshipUpdate(contact.id, relationship === 1 ? 0 : 1);
+              onRelationshipUpdate(contact.id, 1);
+            },
+            onClick: () => {
+              if (relationship === 1) {
+                onRelationshipUpdate(contact.id, 0);
+              }
             }
           }}
         />
@@ -60,6 +77,7 @@ export default function Contact(props: Props) {
             controlType='radio'
             controlProps={{
               name: consistentId,
+              'aria-label': t('UI.ModulesShared.contactsNegative'),
               checked: relationship === -1,
               onMouseEnter: () => setHighlighted(-1),
               onMouseLeave: () => setHighlighted(null),
@@ -78,6 +96,6 @@ export default function Contact(props: Props) {
           onUpdate={onSlotUpdate}
         />
       </span>
-    </div>
+    </RadioControlWrapper>
   );
 }
