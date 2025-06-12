e98b3ad542704fdfbc5339599f3c5598398b7105
Andrey Pissantchev <andrey.pissantchev@protonmail.com>
2025-06-12 17:26:51 +0100

    fix failing tests due to matchMedia not being implemented in jsdom

--- tests/config/fix-js-dom-environment.ts
+++ tests/config/fix-js-dom-environment.ts
@@ -1,11 +1,26 @@
 import JSDOMEnvironment from 'jest-environment-jsdom';
 
-// fixes no structuredClone in jsdom
-// https://github.com/jsdom/jsdom/issues/3363#issuecomment-1467894943
 export default class FixJSDOMEnvironment extends JSDOMEnvironment {
   constructor(...args: ConstructorParameters<typeof JSDOMEnvironment>) {
     super(...args);
 
+    // fixes no structuredClone in jsdom
+    // https://github.com/jsdom/jsdom/issues/3363#issuecomment-1467894943
     this.global.structuredClone = structuredClone;
+
+    // https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom
+    Object.defineProperty(this.global, 'matchMedia', {
+      writable: true,
+      value: (query: string) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: () => {}, // deprecated
+        removeListener: () => {}, // deprecated
+        addEventListener: () => {},
+        removeEventListener: () => {},
+        dispatchEvent: () => {}
+      })
+    });
   }
 }
